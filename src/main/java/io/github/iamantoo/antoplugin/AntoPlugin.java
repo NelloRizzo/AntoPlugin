5e29bf2e1c6f06a4f1ca40ee7a1a089e647f966c
Nello Rizzo <ennerre.biz@gmail.com>
2023-08-26 14:43:45 +0200

    Added FileRepository for local tests.

--- src/main/java/io/github/iamantoo/antoplugin/AntoPlugin.java
+++ src/main/java/io/github/iamantoo/antoplugin/AntoPlugin.java
@@ -1,8 +1,17 @@
 package io.github.iamantoo.antoplugin;
 
+import io.github.iamantoo.antoplugin.data.entities.AntoPlayer;
+import io.github.iamantoo.antoplugin.data.repositories.FileRepository;
+import io.github.iamantoo.antoplugin.data.repositories.Repository;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AntoPlugin extends JavaPlugin {
+
+    private final Repository<AntoPlayer> players = new FileRepository<>("./database.db");
+
+    public Repository<AntoPlayer> getPlayers() {
+        return players;
+    }
 
     @Override
     public void onEnable() {
---
+++ src/main/java/io/github/iamantoo/antoplugin/data/repositories/FileRepository.java
@@ -0,0 +1,64 @@
+package io.github.iamantoo.antoplugin.data.repositories;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class FileRepository<T extends Serializable> implements Repository<T> {
+    private final String fileName;
+
+    private Set<T> items = null;
+
+    public FileRepository(String fileName) {
+        this.fileName = fileName;
+        load();
+    }
+
+    protected void load() {
+        try {
+            try(ObjectInputStream is = new ObjectInputStream(new FileInputStream(fileName))){
+                items = (HashSet<T>)is.readObject();
+            }
+        }catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    protected void save() {
+        try {
+            try(ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fileName))){
+                os.writeObject(items);
+            }
+        }catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    @Override
+    public T create(T entity) {
+        items.add(entity);
+        save();
+        return entity;
+    }
+
+    @Override
+    public T readById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Iterable<T> readAll() {
+        return items.stream().toList();
+    }
+
+    @Override
+    public T update(UUID id, T entity) {
+        return null;
+    }
+
+    @Override
+    public T delete(UUID id) {
+        return null;
+    }
+}
