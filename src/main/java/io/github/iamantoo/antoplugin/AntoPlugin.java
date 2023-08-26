09aae444b0d727066778a5aafff3fca5d1c10004
Nello Rizzo <ennerre.biz@gmail.com>
2023-08-26 13:46:29 +0200

    Initial commit

---
+++ src/main/java/io/github/iamantoo/antoplugin/AntoPlugin.java
@@ -0,0 +1,17 @@
+package io.github.iamantoo.antoplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AntoPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
